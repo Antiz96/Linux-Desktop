450bec182287f39089f6e2bec12cff2f4919bc66
Robin Candau <robincandau@protonmail.com>
2023-12-30 01:47:51 +0100

    Add dynamic wallpaper Wallpapers credit goes to [Matt Vince](https://www.mattvince.com/)

---
+++ Dotfiles/Wallpapers/dynamic-wallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+current_hour=$(date +%H)
+
+if [ "${current_hour}" -ge 8 ] && [ "${current_hour}" -lt 12 ]; then
+	time="morning"
+elif [ "${current_hour}" -ge 12 ] && [ "${current_hour}" -lt 18 ]; then
+	time="day"
+elif [ "${current_hour}" -ge 18 ] && [ "${current_hour}" -lt 22 ]; then
+	time="evening"
+else
+	time="night"
+fi
+
+feh --bg-fill "${HOME}/Pictures/wallpapers/island-${time}.jpg"
